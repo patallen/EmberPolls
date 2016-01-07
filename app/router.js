8e7409d16f1ef4fd4236ad1d8c65473d1a9573af
Patrick Allen <prallen90@gmail.com>
2016-01-07 07:47:53 -0500

    Add beginning to poll app - more to come

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('polls', { path: '/polls' });
+  this.route('poll', { path: '/polls/:poll_id'});
 });
 
 export default Router;
