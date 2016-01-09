771b8bcb20888ef0846cc19a94e07437e3966c53
Patrick Allen <prallen90@gmail.com>
2016-01-09 15:19:18 -0500

    Add user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  username: DS.attr('string'),
+  email: DS.attr('string'),
+  polls: DS.hasMany('choice', {async: true, inverse: 'poll'})
 });
