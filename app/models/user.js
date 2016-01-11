ae84d097b9ece5b81d3b367d4dbbf866ad019731
Patrick Allen <prallen90@gmail.com>
2016-01-11 18:01:49 -0500

    Remove user relationship to Todo for now

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   username: DS.attr('string'),
-  email: DS.attr('string'),
-  polls: DS.hasMany('choice', {async: true, inverse: 'poll'})
+  email: DS.attr('string')
 });
