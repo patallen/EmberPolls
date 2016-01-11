5678e6fa377f3080ed7fa9703cfd3b30951a6e42
Patrick Allen <prallen90@gmail.com>
2016-01-11 18:18:00 -0500

    Add route for single Todo item

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model(params) {
-		return this.store.query('todo', params.todo_id);
+	model: function(params) {
+		return this.store.findRecord('todo', params.todo_id);
 	}
 });
