791dfaee9734eb689362152701666f8563a9d70f
Patrick Allen <prallen90@gmail.com>
2016-01-20 21:02:31 -0500

    Set header content-type to 'application/json' (for now)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-	host: 'http://api.practiceapi.dev'
+	host: 'http://api.practiceapi.dev',
+	headers: {
+        'Content-type': 'application/json'
+	}
 });
