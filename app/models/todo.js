04588d4a42eb1ccb6806ddf5d72cf1cc8baaf317
Patrick Allen <prallen90@gmail.com>
2016-02-09 08:13:24 -0500

    Add relationship between todo and list

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   text: DS.attr('string'),
-  complete: DS.attr('boolean', { defaultValue: false })
+  complete: DS.attr('boolean', { defaultValue: false }),
+  todolist: DS.belongsTo('list'),
 });
